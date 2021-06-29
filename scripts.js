fb5872c64d67ea0900ad2328b4f47f0c56ced65a
cgriffis85 <cgriffis85@yahoo.com>
2021-06-29 14:57:08 -0400

    clarified functions and parameters

--- scripts.js
+++ scripts.js
@@ -1,37 +1,37 @@
 // JTMG subtraction 6/27
 
-function dif(a, b, c) {
-    return a - b - c;
+function subtractHeight(Godzilla, KingKong, Jeff) {
+    return Godzilla - KingKong - Jeff;
 }
 
-const theDif = dif(25, 10, 5);
+const HeightDif = subtractHeight(25, 10, 5);
 
 confirm('this is subtraction')
-alert(theDif); 
+alert(HeightDif); 
 
 // MOSH Control flow exercise 1 6/28
 // 1. return the larger of two numbers
 
 //attempt 1. my code. works, but ugly?
-function max(a, b){
-    if(a>b) return a;
-    if(b>a) return b;
+function maximumGorillaHeight(KingKong, Harambe){
+    if(KingKong>Harambe) return KingKong;
+    if(Harambe>KingKong) return Harambe;
 }
 
-const theMax = max(99, 100);
-confirm('this is the max of two numbers');
+const theMax = maximumGorillaHeight(99, 10);
+confirm('this is the max of two Gorilla Heights');
 alert (theMax);
 
 //attempt 2. if else. works.
 
-function max(a, b){
-    if(a>b) return a;
-    else return b;
+function maximumLizardHeight(Godzilla, Lizzy){
+    if(Godzilla>Lizzy) return Godzilla;
+    else return Lizzy;
 }
 
-const theMax2 = max(27, 10);
-confirm('this is the max of two numbers again');
-alert (theMax2); 
+const maxOfTwoLizards = maximumLizardHeight(27, 10);
+confirm('this is the max of two Lizard Heights');
+alert (maxOfTwoLizards); 
 
 // Mosh's final answer. Conditional operator
 
@@ -39,17 +39,17 @@ function max(a, b){
     return(a>b) ? a : b;
 }
 
-const theMax3 = max(55,3);
+const number = max(55,3);
 confirm('this is the max of two numbers again again');
-alert (theMax3); 
+alert (number); 
 
 //MOSH Control flow exercise 2 6/28
 // 2. Is this a landscape or a portrait?
 
 // Attempt 1. My answer. works.
-const dimensions = isLandscape(80, 190);
+const dimensions = landscapeTrueFalse(80, 190);
 
-function isLandscape(width, height){
+function landscapeTrueFalse(width, height){
     if (width>height) return "Landscape";
     else return "Portrait";
 }
@@ -58,10 +58,11 @@ alert (dimensions);
 
 //Mosh Answer 
 
-function LandscapeOr(width, height){
+function isLandscape(width, height){
     return (width > height)
 }
-const whichOne = LandscapeOr(800, 200);
+const photograph = isLandscape(800, 200);
 confirm('Is this a Landscape?')
-alert (whichOne);
+alert (photograph);
+
 
