a100b3228e12152b0cd06a3812e3a6992d8dfc99
Joshua Gonzalez <jg@jtmg.io>
2021-06-30 08:32:57 -0400

    update question

---
+++ week-26/question.js
@@ -0,0 +1,25 @@
+/*
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+*/
+
+const inQuarterOfYear = (month) => {
+    // Your code here
+
+}
+
+
+//tests 
+if (inQuarterOfYear(3) === 1) {
+    alert('CORRECT')
+} else alert('IN CORRECT!!!!')
+
+if (inQuarterOfYear(8) === 3) {
+    alert('CORRECT')
+} else alert('IN CORRECT!!!!')
+
+
+if (inQuarterOfYear(11) === 4) {
+    alert('CORRECT')
+} else alert('IN CORRECT!!!!')
