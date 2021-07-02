6ef150de3bd4c46b6f1dacfc523c979f858c9093
Joshua Gonzalez <jg@jtmg.io>
2021-07-02 16:29:40 -0400

    update with question 27

---
+++ week-27/question.js
@@ -0,0 +1,26 @@
+/*
+
+Complete the solution so that it reverses all of the words within the string passed in.
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+concepts to learn
+
+	string methods
+	array methods 
+
+	string to array 
+	array to string 
+
+const arr = ["foo", "bar","test"];
+//arr[0] == foo
+//arr[1] == bar
+//arr[2] == test
+*/
+
+'abcdef'.charAt(3)
+
+function reverseWords(str) {
+    return str; // reverse those words
+}
